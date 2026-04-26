2a4da4f3904c70a924e36284bfdcb8651ef0c41a
Hilan <hilan.paganini@gmail.com>
2026-04-26 20:38:35 -0300

    perf: improve performance for successful cases of update and delete

--- server/src/services/ong-service.ts
+++ server/src/services/ong-service.ts
@@ -19,7 +19,9 @@ export const ongService = {
 			const result = await ongRepository.createOng(request, userId);
 			return result[0];
 		} catch {
-			throw new DatabaseError("Erro inesperado ao criar ONG");
+			throw new DatabaseError(
+				"Erro inesperado ao criar ONG. CNPJ, email ou usuário podem já estar cadastrados em outra ONG.",
+			);
 		}
 	},
 	updateOng: async (
@@ -27,21 +29,26 @@ export const ongService = {
 		userId: string,
 		request: Partial<OngRequest>,
 	) => {
-		const ongResult = await ongRepository.getOngAndUserIds(userId);
-		if (ongResult.length === 0) {
-			throw new EntityNotFound("Usuário atual não possui ONG cadastrada");
-		}
-		if (ongResult[0].ongId !== ongId) {
-			throw new ForbiddenError("ONG informada não pertence ao usuário atual");
-		}
 		let updateResult: Awaited<ReturnType<typeof ongRepository.updateOng>>;
 		try {
-			updateResult = await ongRepository.updateOng(ongId, request);
+			updateResult = await ongRepository.updateOng(ongId, userId, request);
 		} catch {
-			throw new DatabaseError("Erro inesperado ao atualizar ONG");
+			throw new DatabaseError(
+				"Erro inesperado ao atualizar ONG. CNPJ ou email podem já estar cadastrados em outra ONG.",
+			);
 		}
 		if (updateResult.length === 0) {
-			throw new EntityNotFound("ONG não encontrada");
+			const userOngResult = await ongRepository.getOngAndUserIds(userId);
+			if (userOngResult.length === 0) {
+				throw new EntityNotFound("Usuário atual não possui ONG cadastrada");
+			}
+			const ongExists = await ongRepository.ongExistsById(ongId);
+			if (!ongExists) {
+				throw new EntityNotFound("ONG não encontrada");
+			}
+			if (userOngResult[0].ongId !== ongId) {
+				throw new ForbiddenError("ONG informada não pertence ao usuário atual");
+			}
 		}
 		return updateResult[0];
 	},
